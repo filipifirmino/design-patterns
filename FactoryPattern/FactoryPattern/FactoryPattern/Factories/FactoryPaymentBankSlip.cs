b842fd47f152d786065c2075a1d7beaf7c8749fc
Filipi Firmino <contatoic3web@gmail.com>
2025-10-27 20:02:46 -0300

    FEAT: Created factory pattern implementation with tax strategy

---
+++ FactoryPattern/FactoryPattern/FactoryPattern/Factories/FactoryPaymentBankSlip.cs
@@ -0,0 +1,18 @@
+﻿using FactoryPattern.Interfaces;
+using FactoryPattern.Models;
+
+namespace FactoryPattern.Factories;
+
+public class FactoryPaymentBankSlip : FactoryPayment
+{
+    private readonly ITax _tax;
+
+    public FactoryPaymentBankSlip(ITax tax)
+    {
+        _tax = tax;
+    }
+    public override IPayment CreatePayment()
+    {
+        return new PaymentBankSlip(_tax);
+    }
+}
---
+++ FactoryPattern/FactoryPattern/FactoryPattern/Models/PaymentPaypal.cs
@@ -0,0 +1,20 @@
+﻿using System.Globalization;
+using FactoryPattern.Interfaces;
+
+namespace FactoryPattern.Models;
+
+public class PaymentPaypal:IPayment
+{
+    private readonly ITax _tax;
+    public PaymentPaypal(ITax tax)
+    {
+        _tax = tax;
+    }
+    public void Pay(decimal value)
+    {
+        var tax = _tax.Calculate(value);
+        Console.WriteLine($"Payment for Paypal with value: R${
+            value.ToString(CultureInfo.InvariantCulture)} with tax of R$ {
+                tax.ToString("F2", CultureInfo.InvariantCulture)}");
+    }
+}
